f5b72e199bd2564ac712957ccf916f028ade1419
Sergio Ravera <sergioravera@hotmail.de>
2026-04-22 11:28:13 +0200

    created separate file for seed data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,37 +1,5 @@
 import { ContactsAPI } from "./contacts_api.js";
-
-function createForm() {
-  const form = document.createElement('form');
-  const nameInput = document.createElement('input');
-  nameInput.type = 'text';
-  nameInput.name = 'full_name';
-  nameInput.value = 'Tester Testo2';
-
-  const emailInput = document.createElement('input');
-  emailInput.type = 'email';
-  emailInput.name = 'email';
-  emailInput.value = 'tester@testo.org';
-
-  const telInput = document.createElement('input');
-  telInput.type = 'tel';
-  telInput.name = 'phone_number';
-  telInput.value = '1234566';
-  form.replaceChildren(nameInput, emailInput, telInput);
-
-  return form;
-}
-
-
-async function testingAPIData() {
-  // const form = createForm();
-  // const json = await ContactsAPI.getAll();
-  // const json = await ContactsAPI.getById(1);
-  // await ContactsAPI.createContact(form);
-
-  // await ContactsAPI.updateContactbyId(4, form);
-  // await ContactsAPI.deleteById(4);
-  console.log(json);
-}
+import { testingAPIData } from "./contacts_api_seed_data.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
